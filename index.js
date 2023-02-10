00f17efd2168687fdcbc2fd1f31e6811e3fd74da
MohammadJavad14 <mrph14@gmail.com>
2023-02-10 15:00:59 +0330

    adding mongodb

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+require("./models/User");
 require("./services/passport");
+const dbUser = process.env.MONGO_USER;
+const dbPassword = process.env.MONGO_PASSWORD;
+const mongURI = "mongodb://localhost/emaily-dev";
+
+mongoose
+  .connect(mongURI)
+  .then(() => console.log("connected to mongodb..."))
+  .catch((err) => console.log("connecting to mongodb failed", err));
 
 const app = express();
 require("./routes/authRoutes")(app);
