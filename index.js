6f9368fb9bcf6b8f9d46072ace779d052e154071
MohammadJavad14 <mrph14@gmail.com>
2023-02-11 17:51:56 +0330

    logging out users

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
+const keys = require("./config/keys");
+require("dotenv").config();
 require("./models/User");
 require("./services/passport");
 
@@ -13,6 +16,17 @@ mongoose
   .catch((err) => console.log("connecting to mongodb failed", err));
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
+
 const keys = require("../config/keys");
 
 const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -13,7 +22,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // the user already exits
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
